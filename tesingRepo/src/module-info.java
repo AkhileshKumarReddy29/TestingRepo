0a3bb15c2c4432a158fe4acacfa4f2c028dc0c45
AkhileshKumarReddy29 <akhileshkumarreddyvuggumudi@gmail.com>
2024-03-29 14:36:53 +0530

    first commit 29-03-2024

---
+++ tesingRepo/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module tesingRepo {
+}
---
+++ tesingRepo/src/tesingRepo/tesingClass.java
@@ -0,0 +1,10 @@
+package tesingRepo;
+
+public class tesingClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
