27ea1c0973df2aa0119be6afce35dc44282c58c8
AkhileshKumarReddy29 <akhileshkumarreddyvuggumudi@gmail.com>
2024-04-30 15:04:35 +0530

    30-04-2024

--- tesingRepo/src/tesingRepo/tesingClass.java
+++ tesingRepo/src/tesingRepo/tesingClass.java
@@ -1,10 +1,28 @@
 package tesingRepo;
+import org.openqa.selenium.By;  
+import org.openqa.selenium.JavascriptExecutor;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
 
 public class tesingClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Hello World");
-
-	}
+		
+		
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Akhilesh Kumar Reddy\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");  
+          
+             // Instantiate a ChromeDriver class.     
+        WebDriver driver=new ChromeDriver();  
+          
+           // Launch Website  
+        driver.navigate().to("http://www.javatpoint.com/");  
+          
+         //Maximize the browser  
+          driver.manage().window().maximize();  
+          Thread.sleep(3000);   
+  
+    }  
+	
 
 }
